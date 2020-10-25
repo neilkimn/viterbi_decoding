97c2981c9708d382e2ff1879c72442001bbd989d
Neil Kim Nielsen <37108154+neilnielsen@users.noreply.github.com>
2020-10-25 23:41:17 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# viterbi_decoding
- 
+# Viterbi Decoding for POS-tagging from scratch
+
+Visualization of the Viterbi algorithm for POS-tagging. Trained on data from the [Universal Dependecy Treebank](https://github.com/UniversalDependencies/UD_Danish-DDT). 
+
+### Requires
+
+```python
+pip install streamlit
+```
+
+### Run
+
+```python
+streamlit run hmm_viterbi_st.py
+```
+
+### Metrics
+
+| **Most likely**         |        |
+| ----------------------- | ------ |
+| Sentence level accuracy | 7.62%  |
+| Word level accuracy     | 78.89% |
+| **Viterbi**             |        |
+| Sentence level accuracy | 16.31% |
+| Word level accuracy     | 76.26% |
+
+![julemanden](julemanden.png)
+
---
+++ hmm_viterbi_st.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import codecs
+import numpy as np
+import myutils
+from hmm import HMM
+from collections import defaultdict
+import pydot
+import networkx as nx
+import matplotlib.pyplot as plt
+from networkx.drawing.nx_pydot import graphviz_layout, write_dot
+
+# load data
+train_data = myutils.read_conll_file("data/da_ddt-ud-train.conllu")
+#dev_data = myutils.read_conll_file("data/da_ddt-ud-dev.conllu")
+
+st.title('Visualisation of the Viterbi decoding algorithm for POS-tagging.')
+
+text = st.text_input('Input text for tagging')
+
+hmm = HMM()
+
+hmm.fit(train_data)
+
+test_text = text.split()
+
+preds = hmm.predict(test_text, method='viterbi')
+
+G = hmm.graph
+
+pos=nx.drawing.nx_agraph.graphviz_layout(
+        G,
+        prog='dot',
+        args='-Grankdir=LR'
+    )
+
+edges, weights = zip(*nx.get_edge_attributes(G,'weight').items())
+
+fig = plt.figure(figsize=(15,15)) 
+
+nx.draw(
+    G,
+    node_size=2000,
+    node_color='#8f8f8f',
+    edgelist=edges,
+    edge_color=weights,
+    width=10,
+    edge_cmap=plt.cm.Blues,
+    arrowsize=1,
+    with_labels=True,
+    labels={n: n for n in G.nodes},
+    font_color='#FFFFFF',
+    font_size=12,
+    pos=pos
+)
+
+labels = nx.get_edge_attributes(G,'weight')
+
+nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
+
+tags = ' '.join(preds[0])
+st.write("POS-tags for input sentence: ", tags)
+
+st.pyplot(fig)
